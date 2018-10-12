382e68ec390721b509aad07276edbb63b75140a9
Guseinov Muslim <gusejnov1999@list.ru>
2018-10-12 20:47:05 +0300

    upgrade screens

--- src/containers/DefaultLayout/DefaultHeader.js
+++ src/containers/DefaultLayout/DefaultHeader.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import { NavLink as RouterLink } from 'react-router-dom';
 import { Badge, DropdownItem, DropdownMenu, DropdownToggle, Nav, NavItem, NavLink } from 'reactstrap';
 import PropTypes from 'prop-types';
 
 import { AppAsideToggler, AppHeaderDropdown, AppNavbarBrand, AppSidebarToggler } from '@coreui/react';
 import logo from '../../assets/img/brand/logo.svg'
-import sygnet from '../../assets/img/brand/sygnet.svg'
+import logoMini from '../../assets/img/brand/logo-mini.svg'
 
 const propTypes = {
   children: PropTypes.node,
@@ -22,21 +23,27 @@ class DefaultHeader extends Component {
       <React.Fragment>
         <AppSidebarToggler className="d-lg-none" display="md" mobile />
         <AppNavbarBrand
-          full={{ src: logo, width: 89, height: 25, alt: 'CoreUI Logo' }}
-          minimized={{ src: sygnet, width: 30, height: 30, alt: 'CoreUI Logo' }}
+          full={{ src: logo, width: 89, height: 25, alt: 'Новое начало' }}
+          minimized={{ src: logoMini, width: 30, height: 30, alt: 'Новое начало' }}
         />
         <AppSidebarToggler className="d-md-down-none" display="lg" />
 
         <Nav className="d-md-down-none" navbar>
-          <NavItem className="px-3">
-            <NavLink href="/">Dashboard</NavLink>
-          </NavItem>
-          <NavItem className="px-3">
-            <NavLink href="#/users">Users</NavLink>
-          </NavItem>
-          <NavItem className="px-3">
-            <NavLink href="#">Settings</NavLink>
-          </NavItem>
+          <RouterLink to="/">
+						<NavItem className="px-3">
+							<NavLink>Рабочая панель</NavLink>
+						</NavItem>
+          </RouterLink>
+					<RouterLink to="/jobs">
+						<NavItem className="px-3">
+							<NavLink>Вакансии</NavLink>
+						</NavItem>
+					</RouterLink>
+          <RouterLink to="/settings">
+						<NavItem className="px-3">
+							<NavLink>Настройки</NavLink>
+						</NavItem>
+          </RouterLink>
         </Nav>
         <Nav className="ml-auto" navbar>
           <NavItem className="d-md-down-none">
