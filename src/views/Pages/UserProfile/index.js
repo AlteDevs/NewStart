23d270949c174fc4093bd14965990b2cdc17edac
Masiya Guseynov <mguseynov@axeta.ru>
2018-10-13 20:52:23 +0300

    Added user-profile page

---
+++ src/views/Pages/UserProfile/index.js
@@ -0,0 +1,72 @@
+import React, { Component, Fragment } from 'react';
+import {
+	Card,
+	CardHeader,
+	CardBody,
+	Button,
+	Form,
+	FormGroup,
+	Label,
+	Input,
+	FormText
+} from 'reactstrap';
+
+class UserProfile extends Component {
+	render() {
+		return (
+			<Fragment>
+				<h1>Профиль пользователя</h1>
+				<Card>
+					<CardHeader>Редактирование профиля</CardHeader>
+					<CardBody>
+						<Form>
+							<FormGroup>
+								<Label for="userLastName">Фамилия</Label>
+								<Input
+									type="text"
+									name="userLastName"
+									id="userLastName"
+									placeholder="Фамилия"
+									value="Пупкин"
+								/>
+							</FormGroup>
+							<FormGroup>
+								<Label for="userFirstName">Имя</Label>
+								<Input
+									type="text"
+									name="userFirstName"
+									id="userFirstName"
+									placeholder="Имя"
+									value="Василий"
+								/>
+							</FormGroup>
+							<FormGroup>
+								<Label for="userMiddleName">Отчество</Label>
+								<Input
+									type="text"
+									name="userMiddleName"
+									id="userMiddleName"
+									placeholder="Отчество"
+									value="Епифанович"
+								/>
+							</FormGroup>
+							<FormGroup>
+								<Label for="userEmail">E-mail</Label>
+								<Input
+									type="text"
+									name="userEmail"
+									id="userEmail"
+									placeholder="E-mail"
+									value="example@mail.ru"
+								/>
+							</FormGroup>
+							<Button color="success">Сохранить</Button>
+						</Form>
+					</CardBody>
+				</Card>
+			</Fragment>
+		);
+	}
+}
+
+export default UserProfile;
